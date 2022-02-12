0c0a2a0334c69fa4d2f7b1d016606792a8bdf44b
FrozenGarvMonkey <garvnair@gmail.com>
2022-02-13 06:35:31 +0800

    Build Error Test Log

---
+++ src/images/db.js
@@ -0,0 +1,8 @@
+const ImageDB = [
+    {"image" : "https://picsum.photos/200"},
+    {"image" : "https://picsum.photos/200"},
+    {"image" : "https://picsum.photos/200"},
+    {"image" : "https://picsum.photos/200"},
+];
+
+export default ImageDB;
