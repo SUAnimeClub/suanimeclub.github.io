67b4c9c4b5ecae979a5db3146b91bce71d879ecb
FrozenGarvMonkey <garvnair@gmail.com>
2022-02-13 06:25:28 +0800

    Added Custom 404 Page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const NotFoundPage = () => (
+  <>
+    <h1 className="text-xl font-bold text-orange-600">404: Not Found</h1>
+    <p>Zamn...</p>
+  </>
+)
+
+export default NotFoundPage
